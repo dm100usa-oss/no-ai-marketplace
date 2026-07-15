9a00aa087a0ec1da4876d979be036452cf26426c
dm100usa-oss <dm100usa@gmail.com>
2026-07-15 11:45:05 -0400

    Add files via upload

--- src/components/FindAccordion.tsx
+++ src/components/FindAccordion.tsx
@@ -96,8 +96,8 @@ export function FindAccordion({
                     src={`/images/find/${tone.icon}.webp`}
                     alt=""
                     aria-hidden="true"
-                    width={64}
-                    height={64}
+                    width={80}
+                    height={80}
                     loading="lazy"
                     decoding="async"
                     className="mb-2 block"
